b049f586ac474b389b69d980577585b9bb563d64
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-20 12:49:44 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3">
     <a class="navbar-brand" href="#">
-        <img src="https://outdoorsecours.fr/wp-content/uploads/2023/07/thumbnail_image001-1-100x100.png" alt="Logo">
+        <img src="https://outdoorsecours.fr/wp-content/uploads/2023/07/thumbnail_image001-1-100x100.png" alt="Logo" style="width: 50px;">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -20,11 +20,11 @@
         <ul class="navbar-nav ml-auto">
             <?php if (isset($_SESSION['user_id'])): ?>
                 <li class="nav-item">
-                    <a class="btn btn-danger" href="logout.php">Déconnexion</a>
+                    <a class="btn btn-danger btn-sm ml-2" href="logout.php">Déconnexion</a>
                 </li>
             <?php else: ?>
                 <li class="nav-item">
-                    <a class="btn btn-primary" href="login.php">Connexion</a>
+                    <a class="btn btn-primary btn-sm ml-2" href="login.php">Connexion</a>
                 </li>
             <?php endif; ?>
         </ul>
