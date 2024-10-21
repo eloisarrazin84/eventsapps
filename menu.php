ccff293c35dbe5bc6e3bad62f30accaaacc57c2c
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-21 11:05:43 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -6,29 +6,29 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <?php if (isset($_SESSION['user_id'])): ?>
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'manage_events.php' ? 'active' : ''; ?>">
-                <a class="nav-link" href="manage_events.php">Gestion des événements</a>
-            </li>
-            <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'manage_users.php' ? 'active' : ''; ?>">
-                <a class="nav-link" href="manage_users.php">Gestion des utilisateurs</a>
-            </li>
+            <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
+                <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'manage_events.php' ? 'active' : ''; ?>">
+                    <a class="nav-link" href="manage_events.php">Gestion des événements</a>
+                </li>
+                <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'manage_users.php' ? 'active' : ''; ?>">
+                    <a class="nav-link" href="manage_users.php">Gestion des utilisateurs</a>
+                </li>
+            <?php endif; ?>
             <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'dashboard.php' ? 'active' : ''; ?>">
                 <a class="nav-link" href="dashboard.php">Tableau de bord</a>
             </li>
         </ul>
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="btn btn-danger btn-sm ml-2" href="logout.php">Déconnexion</a>
-            </li>
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <li class="nav-item">
+                    <a class="btn btn-danger btn-sm ml-2" href="logout.php">Déconnexion</a>
+                </li>
+            <?php else: ?>
+                <li class="nav-item">
+                    <a class="btn btn-primary btn-sm ml-2" href="login.php">Connexion</a>
+                </li>
+            <?php endif; ?>
         </ul>
-        <?php else: ?>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="btn btn-primary btn-sm ml-2" href="login.php">Connexion</a>
-            </li>
-        </ul>
-        <?php endif; ?>
     </div>
 </nav>
