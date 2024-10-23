5855caa647b4840977c704bf83e809dbf20efd02
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-23 09:48:26 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,43 +1,41 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3">
-    <a class="navbar-brand" href="home.php">
+    <a class="navbar-brand" href="#">
         <img src="https://outdoorsecours.fr/wp-content/uploads/2023/07/thumbnail_image001-1-100x100.png" alt="Logo" style="width: 50px;">
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
             <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="adminDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="adminDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fas fa-tools"></i> Administration
                     </a>
-                    <ul class="dropdown-menu" aria-labelledby="adminDropdown">
-                        <li><a class="dropdown-item" href="manage_events.php"><i class="fas fa-calendar-alt"></i> Gestion des événements</a></li>
-                        <li><a class="dropdown-item" href="manage_users.php"><i class="fas fa-users"></i> Gestion des utilisateurs</a></li>
-                    </ul>
+                    <div class="dropdown-menu" aria-labelledby="adminDropdown">
+                        <a class="dropdown-item" href="manage_events.php"><i class="fas fa-calendar-alt"></i> Gestion des événements</a>
+                        <a class="dropdown-item" href="manage_users.php"><i class="fas fa-users"></i> Gestion des utilisateurs</a>
+                    </div>
                 </li>
             <?php endif; ?>
             <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'dashboard.php' ? 'active' : ''; ?>">
                 <a class="nav-link" href="dashboard.php"><i class="fas fa-tachometer-alt"></i> Tableau de bord</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'home.php' ? 'active' : ''; ?>">
                 <a class="nav-link" href="home.php"><i class="fas fa-th-large"></i> Mes applications</a>
             </li>
-            <?php if (isset($_SESSION['user_id'])): ?>
-                <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'profile.php' ? 'active' : ''; ?>">
-                    <a class="nav-link" href="profile.php"><i class="fas fa-user"></i> Mon profil</a>
-                </li>
-            <?php endif; ?>
         </ul>
         <ul class="navbar-nav ml-auto">
             <?php if (isset($_SESSION['user_id'])): ?>
                 <li class="nav-item">
-                    <a class="btn btn-outline-danger btn-sm ml-2" href="logout.php"><i class="fas fa-sign-out-alt"></i> Déconnexion</a>
+                    <a class="nav-link" href="profile.php"><i class="fas fa-user"></i> Mon profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-danger btn-sm ml-2" href="logout.php"><i class="fas fa-sign-out-alt"></i> Déconnexion</a>
                 </li>
             <?php else: ?>
                 <li class="nav-item">
-                    <a class="btn btn-outline-primary btn-sm ml-2" href="login.php"><i class="fas fa-sign-in-alt"></i> Connexion</a>
+                    <a class="btn btn-primary btn-sm ml-2" href="login.php"><i class="fas fa-sign-in-alt"></i> Connexion</a>
                 </li>
             <?php endif; ?>
         </ul>
