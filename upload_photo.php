c108989b5486ccf882d51898c7f3dc8809f065ee
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-23 15:10:01 +0200

    Update upload_photo.php

--- upload_photo.php
+++ upload_photo.php
@@ -11,31 +11,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['profile_picture'])) {
         mkdir($uploadDir, 0777, true);
     }
 
-    // Vérifier si le fichier est bien une image
+    // Vérification de la taille et du type MIME
     $allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
-    if (in_array($file['type'], $allowedTypes)) {
-        $filePath = $uploadDir . basename($file['name']);
-        
-        // Vérifier si le fichier a bien été téléchargé
-        if (move_uploaded_file($file['tmp_name'], $filePath)) {
-            // Mettre à jour la base de données avec le chemin de la photo
-            try {
-                $conn = new PDO("mysql:host=localhost;dbname=outdoorsec", "root", "Lipton2019!");
-                $stmt = $conn->prepare("UPDATE users SET profile_picture = :file_path WHERE id = :user_id");
-                $stmt->bindParam(':file_path', $filePath);
-                $stmt->bindParam(':user_id', $user_id);
-                $stmt->execute();
-                
-                header("Location: profile.php");
-                exit();
-            } catch (PDOException $e) {
-                echo "Erreur : " . $e->getMessage();
-            }
-        } else {
-            echo "Erreur lors du téléchargement de la photo.";
+    if ($file['size'] > 500000) {  // Limite de taille de 500 KB
+        echo "Erreur : le fichier est trop volumineux.";
+        exit();
+    } elseif (!in_array($file['type'], $allowedTypes)) {
+        echo "Erreur : le type de fichier n'est pas autorisé.";
+        exit();
+    }
+
+    $filePath = $uploadDir . basename($file['name']);
+    if (move_uploaded_file($file['tmp_name'], $filePath)) {
+        // Mettre à jour la base de données avec le chemin de la photo
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=outdoorsec", "root", "Lipton2019!");
+            $stmt = $conn->prepare("UPDATE users SET profile_picture = :file_path WHERE id = :user_id");
+            $stmt->bindParam(':file_path', $filePath);
+            $stmt->bindParam(':user_id', $user_id);
+            $stmt->execute();
+            
+            header("Location: profile.php");
+            exit();
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
         }
     } else {
-        echo "Le fichier n'est pas une image valide.";
+        echo "Erreur lors du téléchargement de la photo.";
     }
 }
-?>
