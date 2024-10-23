0910ae8f68d57be6aba1ceb83f5540e659307853
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-23 09:15:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-phpinfo();
+session_start();
+
+// Vérifier si l'utilisateur est déjà connecté
+if (isset($_SESSION['user_id'])) {
+    header("Location: home.php"); // Rediriger vers la page d'accueil si connecté
+    exit();
+} else {
+    header("Location: login.php"); // Rediriger vers la page de connexion si non connecté
+    exit();
+}
 ?>
