1084d4ac522482e6f7e10efaf8548fb13bf4fcc4
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-23 17:04:38 +0200

    Update upload_document.php

--- upload_document.php
+++ upload_document.php
@@ -13,26 +13,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     for ($i = 0; $i < count($files['name']); $i++) {
-        $filePath = $uploadDir . basename($files['name'][$i]);
-        $documentName = !empty($documentNames[$i]) ? htmlspecialchars($documentNames[$i]) : basename($files['name'][$i]);  // Utilise le nom personnalisé ou le nom par défaut
+        if (isset($files['name'][$i]) && $files['error'][$i] === UPLOAD_ERR_OK) {
+            // Utilisez un identifiant unique pour éviter les conflits de noms de fichiers
+            $filePath = $uploadDir . uniqid() . '_' . basename($files['name'][$i]);
+            // Utiliser le nom donné par l'utilisateur ou le nom de fichier par défaut
+            $documentName = !empty($documentNames[$i]) ? htmlspecialchars($documentNames[$i]) : basename($files['name'][$i]);
 
-        if (move_uploaded_file($files['tmp_name'][$i], $filePath)) {
-            // Sauvegarder le document dans la base de données
-            try {
-                $conn = new PDO("mysql:host=localhost;dbname=outdoorsec", "root", "Lipton2019!");
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                $stmt = $conn->prepare("INSERT INTO documents (user_id, document_name, file_path) VALUES (:user_id, :document_name, :file_path)");
-                $stmt->bindParam(':user_id', $user_id);
-                $stmt->bindParam(':document_name', $documentName);
-                $stmt->bindParam(':file_path', $filePath);
-                $stmt->execute();
-
-            } catch (PDOException $e) {
-                echo "Erreur : " . $e->getMessage();
+            if (move_uploaded_file($files['tmp_name'][$i], $filePath)) {
+                // Sauvegarder le document dans la base de données
+                try {
+                    $conn = new PDO("mysql:host=localhost;dbname=outdoorsec", "root", "Lipton2019!");
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $stmt = $conn->prepare("INSERT INTO documents (user_id, document_name, file_path) VALUES (:user_id, :document_name, :file_path)");
+                    $stmt->bindParam(':user_id', $user_id);
+                    $stmt->bindParam(':document_name', $documentName);
+                    $stmt->bindParam(':file_path', $filePath);
+                    $stmt->execute();
+                } catch (PDOException $e) {
+                    echo "Erreur : " . $e->getMessage();
+                }
+            } else {
+                echo "Erreur lors du téléchargement du fichier.";
             }
-        } else {
-            echo "Erreur lors du téléchargement du fichier.";
         }
     }
 
