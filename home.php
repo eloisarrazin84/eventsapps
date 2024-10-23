9371a4b52e3a292cb143dfd140c4fd59f8510b71
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-23 10:14:22 +0200

    Update home.php

--- home.php
+++ home.php
@@ -115,7 +115,6 @@ if (!isset($_SESSION['user_id'])) {
         <div class="app-name">Future App</div>
     </div>
 </div>
-</div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
