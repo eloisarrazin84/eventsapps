eb804a2c9854d2ede0dde7d3f6cbb40ac20c7581
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-23 09:24:35 +0200

    Update login.php

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['username'] = $user['username'];
                 $_SESSION['role'] = $user['role'];
-                header("Location: dashboard.php");
+                header("Location: home.php");
                 exit();
             } else {
                 $error = "Votre compte n'a pas encore été validé par un administrateur.";
