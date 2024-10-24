b7c53fe4f7670b82e1a84cf2e53e5bd3ffcd83d4
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-24 19:25:40 +0200

    Update dashboard_medicaments.php

--- dashboard_medicaments.php
+++ dashboard_medicaments.php
@@ -57,10 +57,10 @@ $categories = $conn->query("SELECT categorie, COUNT(*) as count FROM medicaments
     </div>
 </div>
 
-<!-- Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+<!-- Scripts Bootstrap, jQuery et Leaflet -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
