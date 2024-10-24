874195df09b5e363f6ce8cc2cb3098b29f99088e
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-24 21:00:40 +0200

    Update process_ajout_medicament.php

--- process_ajout_medicament.php
+++ process_ajout_medicament.php
@@ -1,19 +1,17 @@
 <?php
-session_start();
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nom = $_POST['medicament_nom'];
+    $description = $_POST['description'];
+    $quantite = $_POST['quantite'];
+    $date_expiration = $_POST['date_expiration'];
+    $categorie = $_POST['categorie'];
+
     try {
+        // Connexion à la base de données
         $conn = new PDO("mysql:host=localhost;dbname=outdoorsec", "root", "Lipton2019!");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // Récupérer les données du formulaire
-        $nom = htmlspecialchars($_POST['medicament_nom']);
-        $description = htmlspecialchars($_POST['description']);
-        $quantite = intval($_POST['quantite']);
-        $date_expiration = $_POST['date_expiration'];
-        $categorie = htmlspecialchars($_POST['categorie']);
-
-        // Insertion dans la base de données
+        // Insertion des données dans la base de données
         $stmt = $conn->prepare("INSERT INTO medicaments (nom, description, quantite, date_expiration, categorie) VALUES (:nom, :description, :quantite, :date_expiration, :categorie)");
         $stmt->bindParam(':nom', $nom);
         $stmt->bindParam(':description', $description);
@@ -22,8 +20,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $stmt->bindParam(':categorie', $categorie);
 
         $stmt->execute();
-        echo "Médicament ajouté avec succès.";
+
+        // Rediriger vers la page de gestion des médicaments
         header('Location: gestion_medicaments.php');
+        exit();
     } catch (PDOException $e) {
         echo "Erreur : " . $e->getMessage();
     }
