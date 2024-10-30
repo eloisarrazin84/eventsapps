e9ff56277f71cd00db60cedf6a4b6b5dd725eb35
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-30 19:14:00 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'email_helper.php'; // Inclusion de la fonction d'envoi d'email
+require_once 'sendEmail.php'; // Inclusion de la fonction d'envoi d'email
 
 $error = "";
 $success = "";
