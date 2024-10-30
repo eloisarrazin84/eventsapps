a8d8746f5cf3578ef4359c523834d3443b840a80
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-30 19:22:31 +0100

    Update sendEmail.php

--- mail/sendEmail.php
+++ mail/sendEmail.php
@@ -1,45 +1,25 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use Dotenv\Dotenv;
 
 require 'vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+// Charge le fichier .env avec les variables de configuration
+$dotenv = Dotenv\Dotenv::createImmutable('/var/www/html/outdoorsecevent/mail');
 $dotenv->load();
 
-function loadTemplate($templateName, $variables = []) {
-    $templatePath = __DIR__ . "/../email_templates/{$templateName}.html";
-    if (!file_exists($templatePath)) {
-        return false;
-    }
-
-    $templateContent = file_get_contents($templatePath);
-    foreach ($variables as $key => $value) {
-        $templateContent = str_replace("{{{$key}}}", htmlspecialchars($value), $templateContent);
-    }
-
-    return $templateContent;
-}
-
-function sendEmail($to, $subject, $templateName, $variables = [], $from = 'notification@outdoorsecours.fr', $fromName = 'Outdoor Secours') {
+function sendEmail($to, $subject, $body, $from = 'notification@outdoorsecours.fr', $fromName = 'Outdoor Secours') {
     $mail = new PHPMailer(true);
 
     try {
-        // Charger le template d'email
-        $body = loadTemplate($templateName, $variables);
-        if (!$body) {
-            throw new Exception("Template email introuvable : {$templateName}");
-        }
-
         // Configuration de l'email
         $mail->isSMTP();
-        $mail->Host = $_ENV['SMTP_HOST'];
+        $mail->Host = getenv('SMTP_HOST'); 
         $mail->SMTPAuth = true;
-        $mail->Username = $_ENV['SMTP_USER'];
-        $mail->Password = $_ENV['SMTP_PASS'];
+        $mail->Username = getenv('SMTP_USERNAME'); 
+        $mail->Password = getenv('SMTP_PASSWORD'); 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = $_ENV['SMTP_PORT'];
+        $mail->Port = getenv('SMTP_PORT');
 
         $mail->setFrom($from, $fromName);
         $mail->addAddress($to);
