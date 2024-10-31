d4424dde4892a5c55d8e21a831b26cfab70306e0
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-31 09:58:21 +0100

    Update test_email.php

--- test_email.php
+++ test_email.php
@@ -1,22 +1,22 @@
+<?php
 require 'vendor/autoload.php';
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $mail = new PHPMailer(true);
 
 try {
-    $mail->SMTPDebug = 3; // Debug détaillé pour diagnostiquer
+    $mail->SMTPDebug = 3; // Niveau de debug détaillé
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = getenv('SMTP_USERNAME');
-    $mail->Password = getenv('SMTP_PASSWORD');
+    $mail->Username = getenv('SMTP_USERNAME'); // Assurez-vous que la variable d'environnement est définie
+    $mail->Password = getenv('SMTP_PASSWORD'); // Assurez-vous que la variable d'environnement est définie
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
     $mail->setFrom('eloi@bewitness.fr', 'Outdoor Secours');
-    $mail->addAddress('eloi@famillesarrazin.com);
+    $mail->addAddress('eloi@famillesarrazin.com'); // Adresse email corrigée
 
     $mail->isHTML(true);
     $mail->Subject = 'Test';
