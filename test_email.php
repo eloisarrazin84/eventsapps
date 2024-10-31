1420d19ee5d41cc3cb611a0c404c2bb4294cfbad
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-31 09:57:27 +0100

    Update test_email.php

--- test_email.php
+++ test_email.php
@@ -1,19 +1,29 @@
-<?php
-require 'mail/sendEmail.php'; // Chemin vers le fichier sendEmail.php
+require 'vendor/autoload.php';
 
-// Variables pour le test
-$to = 'eloi@famillesarrazin.com'; // Remplacez par votre email pour tester la réception
-$subject = 'Test d’envoi de mail depuis Outdoor Secours';
-$template = 'user_approved'; // Nom du template dans email_templates
-$variables = [
-    'first_name' => 'Prénom Test',
-    'last_name' => 'Nom Test'
-];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Appel de la fonction sendEmail pour le test
-if (sendEmail($to, $subject, $template, $variables)) {
-    echo "Email envoyé avec succès !";
-} else {
-    echo "L'envoi de l'email a échoué.";
+$mail = new PHPMailer(true);
+
+try {
+    $mail->SMTPDebug = 3; // Debug détaillé pour diagnostiquer
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = getenv('SMTP_USERNAME');
+    $mail->Password = getenv('SMTP_PASSWORD');
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->setFrom('eloi@bewitness.fr', 'Outdoor Secours');
+    $mail->addAddress('eloi@famillesarrazin.com);
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Test';
+    $mail->Body = 'Ceci est un test d\'envoi de mail avec PHPMailer';
+
+    $mail->send();
+    echo 'L\'email a été envoyé avec succès.';
+} catch (Exception $e) {
+    echo "L'envoi de l'email a échoué : {$mail->ErrorInfo}";
 }
-?>
