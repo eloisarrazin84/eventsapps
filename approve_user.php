5c13252ecef41e14571546547e66e79571e6e735
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-10-31 09:16:09 +0100

    Update approve_user.php

--- approve_user.php
+++ approve_user.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'email/sendEmail.php'; // Inclusion de la fonction d'envoi d'email
+require_once 'mail/sendEmail.php'; // Inclusion de la fonction d'envoi d'email
 
 // Vérifier si l'utilisateur est un administrateur
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
