4186b0e6062bbdcb34e76c9fa1975945ace285a7
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-11-04 18:49:14 +0100

    Update EmailService.php

--- mail/EmailService.php
+++ mail/EmailService.php
@@ -3,7 +3,8 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '/path/to/vendor/autoload.php'; // Ajustez le chemin vers autoload.php
+require_once __DIR__ . '/../vendor/autoload.php'; // Assurez-vous que votre service d'envoi d'e-mails est correctement inclus
+require_once 'EmailTemplate.php';
 
 class EmailService
 {
@@ -12,29 +13,36 @@ class EmailService
     public function __construct()
     {
         $this->mailer = new PHPMailer(true);
-        $this->mailer->CharSet = 'UTF-8';
+        $this->mailer->CharSet = 'UTF-8'; // Encodage UTF-8 pour le support des accents
 
-        // Configuration SMTP
-        $this->mailer->isSMTP();
-        $this->mailer->Host = 'smtp.example.com';
-        $this->mailer->SMTPAuth = true;
-        $this->mailer->Username = 'your_email@example.com';
-        $this->mailer->Password = 'your_password';
-        $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $this->mailer->Port = 587;
-        $this->mailer->setFrom('from@example.com', 'Your Name');
+        try {
+            // Configuration SMTP
+            $this->mailer->isSMTP();
+            $this->mailer->Host = 'smtp.office365.com';
+            $this->mailer->SMTPAuth = true;
+            $this->mailer->Username = 'notification@outdoorsecours.fr';
+            $this->mailer->Password = 'Lipton2019!';
+            $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $this->mailer->Port = 587;
+
+            // Paramètres de l'expéditeur
+            $this->mailer->setFrom('notification@outdoorsecours.fr', 'Outdoor Secours');
+        } catch (Exception $e) {
+            error_log("Erreur de configuration SMTP : {$e->getMessage()}");
+        }
     }
 
-    public function sendEmail($to, $subject, $templateName, $variables = [])
+    public function sendEmail($to, $subject, $body)
     {
-        $this->mailer->addAddress($to);
-        $this->mailer->isHTML(true);
-        $this->mailer->Subject = $subject;
-
-        // Charger le contenu du template et remplacer les variables
-        $this->mailer->Body = EmailTemplate::loadTemplate($templateName, $variables);
-
-        // Envoi de l'email
-        return $this->mailer->send();
+        try {
+            $this->mailer->addAddress($to); // Destinataire
+            $this->mailer->isHTML(true);
+            $this->mailer->Subject = $subject;
+            $this->mailer->Body = $body;
+            $this->mailer->send();
+        } catch (Exception $e) {
+            error_log("Erreur d'envoi d'email : {$this->mailer->ErrorInfo}");
+        }
     }
 }
+
