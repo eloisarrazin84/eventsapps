b35e9a8bfd72c8a9a3740e9635c67734d0d9dcd1
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-11-04 15:50:49 +0100

    Update dashboard_medicaments.php

--- dashboard_medicaments.php
+++ dashboard_medicaments.php
@@ -57,6 +57,7 @@ foreach ($stockByLocation as $location) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>Dashboard Médicaments</title>
     <style>
