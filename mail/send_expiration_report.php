e83170a07bcc573e8e613a48b5d5cce5fe33dbeb
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-11-04 16:31:04 +0100

    Update send_expiration_report.php

--- mail/send_expiration_report.php
+++ mail/send_expiration_report.php
@@ -1,6 +1,11 @@
 <?php
 require 'EmailService.php'; // Assurez-vous que votre service d'envoi d'e-mails est correctement inclus
 
+function loadEmailTemplate($templateFile, $data) {
+    $template = file_get_contents($templateFile);
+    return str_replace('{{medicaments}}', $data, $template);
+}
+
 function getExpiringMeds($conn) {
     $stmt = $conn->prepare("
         SELECT nom, date_expiration, numero_lot, location_name 
@@ -20,18 +25,20 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $expiringMeds = getExpiringMeds($conn);
 
 if (!empty($expiringMeds)) {
-    $body = "<h1>Récapitulatif des Médicaments Expirants</h1><ul>";
+    $medList = '';
     foreach ($expiringMeds as $med) {
-        $body .= "<li>" . htmlspecialchars($med['nom']) . " - Lot: " . htmlspecialchars($med['numero_lot']) . ", Expire le: " . htmlspecialchars($med['date_expiration']) . ", Lieu: " . htmlspecialchars($med['location_name']) . "</li>";
+        $medList .= "<li>" . htmlspecialchars($med['nom']) . " - Lot: " . htmlspecialchars($med['numero_lot']) . ", Expire le: " . htmlspecialchars($med['date_expiration']) . ", Lieu: " . htmlspecialchars($med['location_name']) . "</li>";
     }
-    $body .= "</ul>";
+
+    // Charger le modèle d'e-mail et y insérer les médicaments
+    $body = loadEmailTemplate('email_templates/med_expiration_report.html', $medList);
 
     // Envoyer l'e-mail
     $emailService = new EmailService();
     $emailService->sendEmail('contact@outdoorsecours.fr', 'Récapitulatif des Médicaments Expirants', $body);
 } else {
-    // Pas de médicaments expirants, vous pouvez décider d'envoyer un e-mail informant qu'il n'y a rien à signaler si nécessaire
-    $body = "<h1>Aucun Médicament N'est en Cours d'Expiration</h1>";
+    // Pas de médicaments expirants
+    $body = loadEmailTemplate('email_templates/med_expiration_report.html', '<li>Aucun médicament n\'est en cours d\'expiration.</li>');
     $emailService = new EmailService();
     $emailService->sendEmail('contact@outdoorsecours.fr', 'Récapitulatif des Médicaments Expirants', $body);
 }
