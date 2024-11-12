24884e5181ab6d539876ed96b81f344d6cb00f67
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-11-12 09:17:01 +0100

    Update forgot_password.php

--- password/forgot_password.php
+++ password/forgot_password.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-require_once __DIR__ . '/mail/EmailService.php'; // Charger le service d'email
+require_once __DIR__ . '/../mail/EmailService.php'; // Charger le service d'email
 
 $error = '';
 $success = '';
