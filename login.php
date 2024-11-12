348d39c3d947918363e50111b803ab55f7c89fad
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-11-12 18:39:49 +0100

    Update login.php

--- login.php
+++ login.php
@@ -114,7 +114,6 @@ else {
 
     </style>
 </head>
-
 <body>
 
     <div class="login-container">
@@ -161,5 +160,4 @@ else {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
-
 </html>
